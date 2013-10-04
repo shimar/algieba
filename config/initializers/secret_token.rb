da1a3bf9dac7519336f54c3a602acce799751b37
shimar <shima01dev@gmail.com>
2013-10-05 00:53:45 +0900

    directories initialized by rails new commands.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Algieba::Application.config.secret_key_base = '7d3bb83e717ae76523470f70838a9d07cb2a1734fc7c16b07ece09caff84379911032de0bdf5a9caa768d21486ef1076dad2399497e1714c0d06726a9abdc720'
