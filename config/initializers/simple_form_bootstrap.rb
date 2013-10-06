43b57c616deb9fab32f32e6bc56c26b70a63a032
shimar <shima01dev@gmail.com>
2013-10-06 23:32:30 +0900

    added the error messages block to the signin form, and adjusted the simple_form configurations in simple_form_bootstrap.rb.

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -6,7 +6,8 @@ SimpleForm.setup do |config|
     b.use :label
     b.wrapper tag: 'div', class: 'controls' do |ba|
       ba.use :input
-      ba.use :error, wrap_with: { tag: 'span', class: 'help-inline' }
+      # ba.use :error, wrap_with: { tag: 'span', class: 'help-inline' }
+      ba.use :error, wrap_with: { tag: 'span', class: 'text-danger' }
       ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
     end
   end
