658eb852a5c4ad8a061eee51a0c49a6b06e81ed4
shimar <shima01dev@gmail.com>
2013-10-07 23:47:10 +0900

    added softwares definition.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131005041636) do
+ActiveRecord::Schema.define(version: 20131007144309) do
+
+  create_table "softwares", force: true do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email"
