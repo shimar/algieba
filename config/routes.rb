2bb970e6cd7bd686b99edaeff7009eb03cb5b8e1
shimar <shima01dev@gmail.com>
2013-10-12 20:38:23 +0900

    added softwares resources.

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Algieba::Application.routes.draw do
   resources :users do
     resources :softwares
   end
+  resources :softwares
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
