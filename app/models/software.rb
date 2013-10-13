e7f3bce34e5a762fefec054696edc94a80ec18c0
shimar <shima01dev@gmail.com>
2013-10-13 22:49:07 +0900

    implemented software model's validations.

--- app/models/software.rb
+++ app/models/software.rb
@@ -1,2 +1,14 @@
 class Software < ActiveRecord::Base
+
+  validates :user_id,
+  presence: true
+
+  validates :name,
+  presence: true,
+  length: { maximum: 100 },
+  uniqueness: { scope: :user_id }
+
+  validates :description,
+  length: { maximum: 255 }
+
 end
