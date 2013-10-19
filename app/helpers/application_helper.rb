938345ae160c473593be051389d996fa61f0066e
shimar <shima01dev@gmail.com>
2013-10-19 21:47:06 +0900

    implement link_to_remove_fields and link_to_add_fields helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,19 @@ module ApplicationHelper
     caption
   end
 
+  def link_to_remove_fields(name, f)
+    f.hidden_field(:_destroy) + link_to(name, onclick: 'remove_fields(this);', class: 'alert-link') do
+      "<span class='glyphicon glyphicon-remove-sign'></span>".html_safe
+    end
+  end
+
+
+  def link_to_add_fields(name, f, association)
+    new_object = f.object.class.reflect_on_association(association).klass.new
+    fields = f.simple_fields_for(association, new_object, child_index: "new_#{association}") do |builder|
+      render(association.to_s.singularize + '_fields', f: builder)
+    end
+    link_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")")
+  end
+
 end
