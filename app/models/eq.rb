28fecaae33478a3220774e31e639d2a1450054b6
shimar <shima01dev@gmail.com>
2013-10-23 00:30:15 +0900

    implement #9.
    define the Eq model as subclass of transactional function.

---
+++ app/models/eq.rb
@@ -0,0 +1,13 @@
+class Eq < TransactionalFunction
+
+  @@function_points = {
+    low:     3,
+    average: 4,
+    high:    6,
+  }.freeze
+
+  def function_point
+    @@function_points[complexity]
+  end
+
+end
---
+++ spec/models/eq_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe Eq do
+
+  describe :function_point do
+    before(:each) do
+      @model = Eq.new
+    end
+
+    context "when its complexity is :low," do
+      it "returns 3 as a function point." do
+        @model.stub(:complexity).and_return :low
+        @model.function_point.should == 3
+      end
+    end
+
+    context "when its complexity is :average," do
+      it "returns 4 as a function point." do
+        @model.stub(:complexity).and_return :average
+        @model.function_point.should == 4
+      end
+    end
+
+    context "when its complexity is :high," do
+      it "returns 6 as a function piont." do
+        @model.stub(:complexity).and_return :high
+        @model.function_point.should == 6
+      end
+    end
+
+  end
+
+end
