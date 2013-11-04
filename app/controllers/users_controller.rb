4802dfcca59d843edabcde9005e510a347d652b5
shimar <shima01dev@gmail.com>
2013-11-04 14:31:54 +0900

    fix bug#20. user can't access the profile of someone else.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -70,7 +70,8 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+      render status: :forbidden, text: 'forbidden' unless params[:id] == session[:user_id].to_s
+      @user = current_user
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
