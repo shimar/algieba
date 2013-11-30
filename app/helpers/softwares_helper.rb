59badf61841059a414169cc914a3407177cfdc05
shimar <shima01dev@gmail.com>
2013-11-30 23:16:23 +0900

    display a message when the software has no description in top page.

--- app/helpers/softwares_helper.rb
+++ app/helpers/softwares_helper.rb
@@ -1,5 +1,13 @@
 module SoftwaresHelper
 
+  def software_description(software)
+    if software.description.present?
+      software.description
+    else
+      "<span class='text-muted'>no description.</span>".html_safe
+    end
+  end
+
   def render_measure_method(software)
     # FIXME to use i18n.
     text = Software.measure_methods.key(software.measure_method).to_s
